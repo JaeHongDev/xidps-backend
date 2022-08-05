900f03d6ecf9344e0187a00c609dfb38933b7cb2
jaehong <println@kakao.com>
2022-08-05 14:54:40 +0900

    Initial commit

---
+++ src/main/java/com/example/xidpsbackend/XidpsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.xidpsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XidpsBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XidpsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xidpsbackend/XidpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xidpsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XidpsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
